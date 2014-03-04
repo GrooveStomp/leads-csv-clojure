2cbf33f17ed4c53e8860abeda6026634e48c1c43
GrooveStomp <aaron.oman@gmail.com>
2014-03-04 10:50:42 -0800

    Most of the implementation is done.  Still need to test and validate. :P

--- project.clj
+++ project.clj
@@ -2,8 +2,10 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data-json "0.2.4"]
                  [compojure "1.1.6"]
                  [korma "0.3.0-RC6"]
+                 [mysql/mysql-connector-java "5.1.6"]
                  [clojure-csv/clojure-csv "2.0.1"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler app.main/app}
--- src/app/main.clj
+++ src/app/main.clj
@@ -1,11 +1,50 @@
 (ns app.main
-  (:use compojure.core)
+  (:use compojure.core
+        korma.db
+        korma.core
+        korma.mysql)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [clojure.data.json :as json]))
+
+(defdb db (mysql {:db "lp_webapp"
+                  :user "root"
+                  :password "password"}))
+
+(defentity form_submissions)
+
+(defn leads-from-page-id [page-id]
+  (select form_submissions
+          (where {:page_uuid page-id})))
+
+(defn leads-as-hashes [leads]
+  (map lead-to-hash leads))
+
+(defn lead-to-hash [lead]
+  (apply hash-map (conj [:variant (:variant_id lead)
+                         :ip_address (:submitter_ip lead)
+                         :page_uuid (:page_uuid lead)
+                         :date_submitted (:date_submitted lead)
+                         :time_submitted (:time_submitted lead)]
+                        (keys (json/read-str (:form_data lead))))))
+
+(defn leads-headers [hash-leads]
+  (let [header-mess (distinct (flatten (map keys hash-leads)))]
+    (-> [:page_uuid :date_submitted :time_submitted :variant :ip_address]
+      (concat header-mess)
+      (distinct))))
+
+(defn lead-data-in-order [headers hash-lead]
+  (map #(hash-lead %) headers))
+
+(defn leads-as-csv-arrays [headers hash-leads]
+  (conj [(leads-headers hash-leads)]
+        (map leads-data-in-order hash-leads)))
 
 (defroutes app-routes
   (GET "/pages/:page_id" [page_id]
-       (str "You chose page: " page_id))
+       (let [leads (leads-from-page-id page_id)]
+         (str "Leads: " leads)))
   (route/resources "/")
   (route/not-found "Not Found"))
 
